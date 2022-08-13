703a8b322d4a92e24049949161587d14e1290c23
Subhasish Padhee <44895248+Subhasish-143@users.noreply.github.com>
2022-08-13 16:16:49 +0530

    Add files via upload

---
+++ NotePad/src/com/NotePad/NotePad.java
@@ -0,0 +1,16 @@
+package com.NotePad;
+
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class NotePad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
